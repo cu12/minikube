13157c82049ab94ecdd68c19bb42ca51f6b6877f
Domonkos Cinke <domonkos.cinke@namecheap.com>
2017-06-19 20:22:23 +0200

    Fix build

--- deploy/iso/minikube-iso/package/glusterfs/glusterfs.mk
+++ deploy/iso/minikube-iso/package/glusterfs/glusterfs.mk
@@ -12,7 +12,11 @@ GLUSTERFS_SITE = https://download.gluster.org/pub/gluster/glusterfs/$(GLUSTERFS_
 GLUSTERFS_SOURCE = glusterfs-$(GLUSTERFS_VERSION).tar.gz
 GLUSTERFS_DEPENDENCIES = host-flex host-bison openssl acl
 GLUSTERFS_CONF_OPTS = --disable-tiering
-GLUSTERFS_CONF_ENV += ac_cv_file__etc_debian_version="/etc/debian_version"
+
+GLUSTERFS_CONF_ENV = \
+  ac_cv_file__etc_debian_version=no \
+  ac_cv_file__etc_redhat_release=no \
+  ac_cv_file__etc_SuSE_release=no
 
 define GLUSTERFS_RUN_AUTOGEN
     cd $(@D) && PATH=$(BR_PATH) ./autogen.sh
