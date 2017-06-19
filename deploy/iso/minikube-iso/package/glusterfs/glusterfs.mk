113aab1868747fc5afed3fb71fb4749c5c2a19fa
Domonkos Cinke <domonkos.cinke@namecheap.com>
2017-06-19 20:52:19 +0200

    Fix build

--- deploy/iso/minikube-iso/package/glusterfs/glusterfs.mk
+++ deploy/iso/minikube-iso/package/glusterfs/glusterfs.mk
@@ -10,8 +10,8 @@ GLUSTERFS_BUILD = 0
 GLUSTERFS_VERSION = $(GLUSTERFS_MAJOR).$(GLUSTERFS_MINOR).$(GLUSTERFS_BUILD)
 GLUSTERFS_SITE = https://download.gluster.org/pub/gluster/glusterfs/$(GLUSTERFS_MAJOR).$(GLUSTERFS_MINOR)/$(GLUSTERFS_VERSION)
 GLUSTERFS_SOURCE = glusterfs-$(GLUSTERFS_VERSION).tar.gz
-GLUSTERFS_DEPENDENCIES = host-flex host-bison acl openssl liburcu
-GLUSTERFS_CONF_OPTS = --disable-tiering
+GLUSTERFS_DEPENDENCIES = host-flex host-bison acl openssl liburcu sqlite
+GLUSTERFS_AUTORECONF = YES
 
 GLUSTERFS_CONF_ENV = \
   ac_cv_file__etc_debian_version=no \
