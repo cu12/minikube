c3fb5dfdbeafb218bb975b504a84c8fe14846f29
Domonkos Cinke <domonkos.cinke@namecheap.com>
2017-06-19 19:30:49 +0200

    Fix build

--- deploy/iso/minikube-iso/package/glusterfs/glusterfs.mk
+++ deploy/iso/minikube-iso/package/glusterfs/glusterfs.mk
@@ -11,28 +11,6 @@ GLUSTERFS_VERSION = $(GLUSTERFS_MAJOR).$(GLUSTERFS_MINOR).$(GLUSTERFS_BUILD)
 GLUSTERFS_SITE = https://download.gluster.org/pub/gluster/glusterfs/$(GLUSTERFS_MAJOR).$(GLUSTERFS_MINOR)/$(GLUSTERFS_VERSION)
 GLUSTERFS_SOURCE = glusterfs-$(GLUSTERFS_VERSION).tar.gz
 GLUSTERFS_DEPENDENCIES = host-flex host-bison openssl acl
-
-
-
-define GLUSTERFS_RUN_AUTOGEN
-    cd $(@D) && PATH=$(BR_PATH) ./autogen.sh
-endef
-GLUSTERFS_PRE_CONFIGURE_HOOKS += GLUSTERFS_RUN_AUTOGEN
-
-define GLUSTERFS_CONFIGURE_CMDS
-  cd $(@D) && PATH=$(BR_PATH) \
-  LDFLAGS="$(HOST_LDFLAGS)" \
-    ./configure
-endef
-
-define GLUSTERFS_BUILD_CMDS
-  $(TARGET_CONFIGURE_OPTS) $(MAKE) -C $(@D)
-endef
-
-define GLUSTERFS_
-  $(INSTALL) -D -m 0755 \
-    $(@D)/glusterfs \
-    $(TARGET_DIR)/usr/sbin/glusterfs
-endef
+GLUSTERFS_AUTORECONF=YES
 
 $(eval $(generic-package))
