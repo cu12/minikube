b62721e2cb8b9614140ea64f2850be28d287fbeb
Domonkos Cinke <domonkos.cinke@namecheap.com>
2017-06-19 19:17:12 +0200

    Fix build

--- deploy/iso/minikube-iso/package/glusterfs/glusterfs.mk
+++ deploy/iso/minikube-iso/package/glusterfs/glusterfs.mk
@@ -11,6 +11,8 @@ GLUSTERFS_VERSION = $(GLUSTERFS_MAJOR).$(GLUSTERFS_MINOR).$(GLUSTERFS_BUILD)
 GLUSTERFS_SITE = https://download.gluster.org/pub/gluster/glusterfs/$(GLUSTERFS_MAJOR).$(GLUSTERFS_MINOR)/$(GLUSTERFS_VERSION)
 GLUSTERFS_SOURCE = glusterfs-$(GLUSTERFS_VERSION).tar.gz
 GLUSTERFS_DEPENDENCIES = host-flex host-bison openssl
+GLUSTERFS_CONF_ENV += CPPFLAGS=-I$(STAGING_DIR)/usr/include"
+
 
 define GLUSTERFS_RUN_AUTOGEN
     cd $(@D) && PATH=$(BR_PATH) ./autogen.sh
@@ -20,8 +22,7 @@ GLUSTERFS_PRE_CONFIGURE_HOOKS += GLUSTERFS_RUN_AUTOGEN
 define GLUSTERFS_CONFIGURE_CMDS
   cd $(@D) && PATH=$(BR_PATH) \
   LDFLAGS="$(HOST_LDFLAGS)" \
-  ./configure \
-    --includedir=$(STAGING_DIR)/usr/include
+  ./configure
 endef
 
 define GLUSTERFS_BUILD_CMDS
