f44ecc36d634cbabc2a791762d502aa37e054e4f
Domonkos Cinke <domonkos.cinke@namecheap.com>
2017-06-19 22:36:42 +0200

    Fix build

--- deploy/iso/minikube-iso/package/glusterfs/glusterfs.mk
+++ deploy/iso/minikube-iso/package/glusterfs/glusterfs.mk
@@ -11,15 +11,12 @@ GLUSTERFS_VERSION = $(GLUSTERFS_MAJOR).$(GLUSTERFS_MINOR).$(GLUSTERFS_BUILD)
 GLUSTERFS_SITE = https://download.gluster.org/pub/gluster/glusterfs/$(GLUSTERFS_MAJOR).$(GLUSTERFS_MINOR)/$(GLUSTERFS_VERSION)
 GLUSTERFS_SOURCE = glusterfs-$(GLUSTERFS_VERSION).tar.gz
 GLUSTERFS_DEPENDENCIES = acl openssl liburcu sqlite
+GLUSTERFS_AUTORECONF = YES
 
 GLUSTERFS_CONF_ENV = \
   ac_cv_file__etc_debian_version=no \
   ac_cv_file__etc_redhat_release=no \
   ac_cv_file__etc_SuSE_release=no
 
-define GLUSTERFS_RUN_AUTOGEN
-    cd $(@D) && PATH=$(BR_PATH) ./autogen.sh
-endef
-GLUSTERFS_PRE_CONFIGURE_HOOKS += GLUSTERFS_RUN_AUTOGEN
 
-$(eval $(host-autotools-package))
+$(eval $(autotools-package))
